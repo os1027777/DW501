e8af21dd273a8e721d01e52c11b7de88c5d3f80f
mindoong2 <alswjdgkgkgh@naver.com>
2022-10-27 12:51:37 +0900

    feat : 1027

---
+++ HTML/오전수업/1027/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once("head.html")   ?>
+
+
+<body>
+    테스트중
+</body>
+
+</html>
---
+++ htdocs/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once("head.html")   ?>
+
+<body>
+    <div id="wrap">
+        <?php require_once("header.html") ?>
+
+    </div>
+</body>
+
+</html>
